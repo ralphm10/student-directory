229297ec72befee8246cf6424fc92e6022b32a37
Ralph Mallett <rmallett12@gmail.com>
2020-06-30 12:58:30 +0000

    Add save students method to write to csv file

--- directory.rb
+++ directory.rb
@@ -6,6 +6,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else 
@@ -23,6 +25,7 @@ end
 def print_menu    
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end 
 
@@ -35,7 +38,6 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -50,7 +52,6 @@ def input_students
   @students
 end 
   
-  
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -64,6 +65,18 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end 
+
+def save_students
+  # open the file for writing 
+  file = File.open("students.csv","w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line =  student_data.join(",")
+    file.puts csv_line
+  end 
+  file.close
 end 
 
 interactive_menu
